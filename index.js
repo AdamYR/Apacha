59387b48a140834a1d74928b12d1f3f684cae7d6
AdamYR <642544017@qq.com>
2018-11-25 15:47:24 +0800

    实现了文件不存在错误提示

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 // 导入模块
-const fs=require('fs');
-const http=require('http');
-const path=require('path');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 //记录网站根目录
-let rootPath=path.join(__dirname,"www");
+let rootPath = path.join(__dirname, "www");
 
-let server=http.createServer((request,response)=>{
+let server = http.createServer((request, response) => {
     //生成地址
-    // let targetPath=path.join(rootPath,request.url);
-    response.end('hello');
+    let targetPath = path.join(rootPath, request.url);
+    // 判断路径是否存在
+    if (fs.existsSync(targetPath)) {
+        // 存在
+        // 文件还是文件夹
+        let stats = fs.stat(targetPath, () => { });
+    } else {
+        //设置状态码
+        response.statusCode = 404;
+        //设置字符集
+        response.setHeader('content-type','text/html;charset=utf-8');
+        //不存在
+        response.end(`
+          <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+              <html>
+              <head>
+                  <title>Index of/ </title>
+              </head>
+              <body>
+                  <h1>Index of ${request.url}</h1>
+                  <ul>
+                      这是一个很寂寞的天
+                  </ul>
+              </body>
+              </html>
+          `);
+    }
+    // response.end('hello'); 
 })
-server.listen(80,'127.0.0.1',()=>{
+server.listen(80, '127.0.0.1', () => {
     console.log('开启成功');
 })
